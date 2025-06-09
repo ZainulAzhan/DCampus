7bac7010a209e6e2f707e048560aaea34cfeaa10
Zainul Azhan <zainulazhan2014@gmail.com>
2025-06-10 01:18:15 +0800

    add solution folders and projects

---
+++ src/Core/dcampus.Domain/Models/Academics/Programme.cs
@@ -0,0 +1,24 @@
+﻿using dcampus.Domain.Abstractions;
+
+namespace dcampus.Domain.Models.Academics;
+
+public class Programme : Entity<Guid>
+{
+    public string Code { get; private set; } = default!;
+    public string Name { get; private set; } = default!;
+    public string Description { get; private set; } = default!;
+
+    public static Programme Create(Guid id, string code, string name, string description)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(code);
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(description);
+        return new Programme
+        {
+            Id = id,
+            Code = code,
+            Name = name,
+            Description = description
+        };
+    }
+}
